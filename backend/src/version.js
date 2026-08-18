62ddaf4c89753e51ac8c5547d325f07947549e28
wza0126 <306128186+wza0126@users.noreply.github.com>
2026-08-18 21:03:41 +0800

    chore: 更新版本号至 1.3.2，更新提交 ID 和构建时间

--- backend/src/version.js
+++ backend/src/version.js
@@ -1,6 +1,6 @@
 // 本文件由 scripts/gen-version.js 自动生成，请勿手工修改
 module.exports = {
-  version: "1.3.1",
-  commit: "c01e3e1",
-  buildTime: "2026-08-18T09:53:07.699Z",
+  version: "1.3.2",
+  commit: "367edf6",
+  buildTime: "2026-08-18T10:53:16.067Z",
 };
