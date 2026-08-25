5d1852bca6a91ba804e2977159fb62e084315a7b
wza0126 <306128186+wza0126@users.noreply.github.com>
2026-08-25 14:38:41 +0800

    chore: 更新版本构建元数据

--- backend/src/version.js
+++ backend/src/version.js
@@ -1,6 +1,6 @@
 // 本文件由 scripts/gen-version.js 自动生成，请勿手工修改
 module.exports = {
   version: "1.3.6",
-  commit: "fd39321",
-  buildTime: "2026-08-25T05:49:36.071Z",
+  commit: "7ee5439",
+  buildTime: "2026-08-25T06:02:43.513Z",
 };
