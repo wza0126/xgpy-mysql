9ffd99c18b6f7192b08a78813f9358821d470654
wza0126 <306128186+wza0126@users.noreply.github.com>
2026-09-05 15:40:13 +0800

    chore: 版本升至 2.1.0

--- backend/src/version.js
+++ backend/src/version.js
@@ -1,6 +1,6 @@
 // 本文件由 scripts/gen-version.js 自动生成，请勿手工修改
 module.exports = {
-  version: "2.0.2",
-  commit: "3f14897",
-  buildTime: "2026-08-27T10:49:05.919Z",
+  version: "2.1.0",
+  commit: "72d64c5",
+  buildTime: "2026-09-05T07:39:37.434Z",
 };
